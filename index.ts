ba19a44d7ae50a1a7cf345d223db5e11e8942225
prophecyengineer <hey.charlotte@icloud.com>
2025-02-26 22:52:24 +0000

    npm set up

---
+++ index.ts
@@ -0,0 +1,5 @@
+// Export all components
+export * from "./registry/apple-button/apple-button";
+export * from "./registry/hello-world/hello-world";
+export * from "./registry/example-form/example-form";
+// Add other components as needed
