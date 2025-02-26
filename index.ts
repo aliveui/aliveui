94fb85ba6cb914254a9d0939cc9ae081d4f18e60
prophecyengineer <hey.charlotte@icloud.com>
2025-02-26 23:14:26 +0000

    initial

--- index.ts
+++ index.ts
@@ -1,5 +1,2 @@
 // Export all components
 export * from "./registry/apple-button/apple-button";
-export * from "./registry/hello-world/hello-world";
-export * from "./registry/example-form/example-form";
-// Add other components as needed
